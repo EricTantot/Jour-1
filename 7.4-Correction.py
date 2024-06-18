ea53bd66187fa55c891f85ac21c93778e885f2a0
EricTantot <eric.tantot08@gmail.com>
2024-06-18 09:44:32 +0200

    essai espace

--- 7.4-Correction.py
+++ 7.4-Correction.py
@@ -2,5 +2,5 @@ def corrDyslexie(texte):
     correction = texte.replace("ua", "au")
     print(f"Voici votre texte corrigé : \n{correction}")
 
-texte = input("Entrer votre texte: ")
+texte = input("Entrer votre texte : ")
 corrDyslexie(texte)
